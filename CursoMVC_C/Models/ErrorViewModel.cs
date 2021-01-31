deed540e4faac98d00e3ce158ad43973970edb4a
JonathasTorquato <jonathastorquato26@gmail.com>
2021-01-31 11:14:46 -0300

    Projeto MVC

---
+++ CursoMVC_C/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CursoMVC_C.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ CursoMVC_C/Models/Produto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CursoMVC_C.Models
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        [Display(Name = "Descrição")]
+        public string Descricao { get; set; }
+        [Range(0,10, ErrorMessage ="Quantidade deve ser um valor entre 0 e 10.")]
+        public int Quantidade { get; set; }
+        public int CategoriaId { get; set; }
+        [Display(Name = "Categoria")]
+        public Categoria categoria { get; set; }
+       
+    }
+}
